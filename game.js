4fa50f546d095853f28f6e642c5b202d9c9ada80
Harshul Jain <harshulgangwal02@gmail.com>
2022-08-07 01:17:16 +0530

    Added Wrong answer

--- game.js
+++ game.js
@@ -4,16 +4,58 @@ var gamePattern=[];
 
 var userClickedPattern=[];
 
+var started=false;
+var level=0;
+
+$(document).keypress(function(){
+    if(!started)
+    {
+        $("#level-title").text("Level"+level);
+        nextSequence();
+        started=true;
+    }
+});
+
 $(".btn").click(function(){
     var userChosenColor=$(this).attr("id");
     
     userClickedPattern.push(userChosenColor);
 
-    console.log(userClickedPattern);
+    // console.log(userClickedPattern);
     playSound(userChosenColor);
+    animatePress(userChosenColor);
+    checkAnswer(userClickedPattern.length -1);
 });
 
+function checkAnswer(currentLevel)
+{
+    if(userClickedPattern[currentLevel]===gamePattern[currentLevel])
+    {
+        if(userClickedPattern.length===gamePattern.length)
+        {
+            setTimeout(function(){
+                nextSequence();
+            },1000);
+        }
+    }
+    else{
+        // console.log("wrong");
+        playSound("wrong");
+
+        $("body").addClass("game-over");
+
+        setTimeout(function(){
+            $("body").removeClass("game-over");
+        },200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+    }
+}
+
 function nextSequence(){
+    userClickedPattern = [];
+
+    level++;
+    $("#level-title").text("Level " + level);
     var randomNumber=Math.floor(Math.random()*4);
     var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
@@ -42,3 +84,5 @@ function animatePress(currentColour)
         $("#"+currentColour).removeClass("pressed");
     },100);
 }
+
+
