8abfb99d612595a8586cf3e4c381a6f360529376
Harshul Jain <harshulgangwal02@gmail.com>
2022-08-06 23:19:44 +0530

    Added animated flash

--- game.js
+++ game.js
@@ -6,9 +6,10 @@ var randomNumber;
 
 function nextSequence(){
     randomNumber=Math.floor(Math.random()*4);
-    console.log(randomNumber);
+    // console.log(randomNumber);
+    var randomChosenColour=buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
 }
 nextSequence();
-var randomChosenColour=buttonColours[randomNumber];
 // console.log(randomChosenColour);
-gamePattern.push(randomChosenColour);
