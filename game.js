a124e1c15873d96dbfe33b82b43a4ba456e09d3e
Harshul Jain <harshulgangwal02@gmail.com>
2022-08-07 00:16:41 +0530

    Added Sound

--- game.js
+++ game.js
@@ -1,15 +1,34 @@
-var gamePattern=[];
-
 var buttonColours = ["red", "blue", "green", "yellow"];
 
-var randomNumber;
+var gamePattern=[];
+
+var userClickedPattern=[];
+
+$(".btn").click(function(){
+    var userChosenColor=$(this).attr("id");
+    
+    userClickedPattern.push(userChosenColor);
+
+    console.log(userClickedPattern);
+    playSound(userChosenColor);
+});
 
 function nextSequence(){
-    randomNumber=Math.floor(Math.random()*4);
-    // console.log(randomNumber);
-    var randomChosenColour=buttonColours[randomNumber];
+    var randomNumber=Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
     gamePattern.push(randomChosenColour);
-    $("#"+randomChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    playSound(randomChosenColour);
+    // audio.play();
+    
 }
-nextSequence();
+
 // console.log(randomChosenColour);
+
+function playSound(name)
+{
+    var audio=new Audio("sounds/"+name+".mp3");
+    audio.play();
+}
